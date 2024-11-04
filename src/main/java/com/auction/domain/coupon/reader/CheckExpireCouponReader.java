33a1622dea365d2b7075c8349119c079e6df24c2
kim-na-ram <snrneh3@naver.com>
2024-11-04 15:54:10 +0900

    update(coupon) : 쿠폰 만료 처리 JPA -> JDBC, chunk size 최적화

--- src/main/java/com/auction/domain/coupon/reader/CheckExpireCouponReader.java
+++ src/main/java/com/auction/domain/coupon/reader/CheckExpireCouponReader.java
@@ -1,40 +1,85 @@
 package com.auction.domain.coupon.reader;
 
+import com.auction.domain.coupon.dto.CouponDto;
 import com.auction.domain.coupon.entity.CouponUser;
 import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
 import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.PagingQueryProvider;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
 import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
+import javax.sql.DataSource;
 import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class CheckExpireCouponReader {
+    private final DataSource dataSource;
+
+//    @Bean
+//    @StepScope
+//    public JpaPagingItemReader<CouponUser> getExpireCouponReader(
+//            EntityManagerFactory entityManagerFactory,
+//            @Value("#{jobParameters['expireAt']}") LocalDate expireAt
+//    ) {
+//        HashMap<String, Object> parameters = new HashMap<>();
+//        parameters.put("expireAt", expireAt);
+//
+//        return new JpaPagingItemReaderBuilder<CouponUser>()
+//                .name("couponIdPagingReader")
+//                .pageSize(1000)
+//                .entityManagerFactory(entityManagerFactory)
+//                .queryString("SELECT u FROM Coupon c JOIN CouponUser u ON c.id = u.coupon.id " +
+//                        "WHERE c.expireAt = :expireAt ORDER BY u.id")
+//                .parameterValues(parameters)
+//                .build();
+//    }
+
     @Bean
     @StepScope
-    public JpaPagingItemReader<CouponUser> getExpireCouponReader(
-            EntityManagerFactory entityManagerFactory,
+    public JdbcPagingItemReader<CouponDto> getExpireCouponReader(
+            PagingQueryProvider queryProvider,
             @Value("#{jobParameters['expireAt']}") LocalDate expireAt
     ) {
         HashMap<String, Object> parameters = new HashMap<>();
-        parameters.put("expireAt", expireAt);
+        parameters.put("expire_at", expireAt);
 
-        return new JpaPagingItemReaderBuilder<CouponUser>()
+        return new JdbcPagingItemReaderBuilder<CouponDto>()
                 .name("couponIdPagingReader")
                 .pageSize(1000)
-                .entityManagerFactory(entityManagerFactory)
-                .queryString("SELECT u FROM Coupon c JOIN CouponUser u ON c.id = u.coupon.id " +
-                        "WHERE c.expireAt = :expireAt ORDER BY u.id")
+                .fetchSize(1000)
+                .dataSource(dataSource)
+                .rowMapper(new BeanPropertyRowMapper<>(CouponDto.class))
+                .queryProvider(queryProvider)
                 .parameterValues(parameters)
                 .build();
     }
 
+    @Bean
+    public PagingQueryProvider queryProvider() throws Exception {
+        SqlPagingQueryProviderFactoryBean queryProvider = new SqlPagingQueryProviderFactoryBean();
+        queryProvider.setDataSource(dataSource);
+        queryProvider.setSelectClause("SELECT u.id");
+        queryProvider.setFromClause("FROM coupon c JOIN coupon_user u ON c.id = u.coupon_id");
+        queryProvider.setWhereClause("WHERE c.expire_at = :expire_at AND u.is_available = true");
+
+        Map<String, Order> sortKeys = new HashMap<>(1);
+        sortKeys.put("u.id", Order.ASCENDING);
+
+        queryProvider.setSortKeys(sortKeys);
+        return queryProvider.getObject();
+    }
 }
