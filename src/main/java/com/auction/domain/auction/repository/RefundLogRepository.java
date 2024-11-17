f73e75f5bb1bb858b827505d14291dba0ba2afde
kim-na-ram <snrneh3@naver.com>
2024-11-18 02:47:17 +0900

    del(refund) : msa 전환으로 인한 환불 배치 제거

--- src/main/java/com/auction/domain/auction/repository/RefundLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.auction.domain.auction.repository;
-
-import com.auction.domain.auction.entity.RefundLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RefundLogRepository extends JpaRepository<RefundLog, Long> {
-}
