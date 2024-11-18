f6705f2e5758d6c9c57c5418b2614ea297be6baf
kim-na-ram <snrneh3@naver.com>
2024-11-18 18:20:37 +0900

    refactor(coupon) : 불필요한 설정 제거, 상수화

--- src/main/java/com/auction/domain/coupon/reader/CheckExpireCouponReader.java
+++ src/main/java/com/auction/domain/coupon/reader/CheckExpireCouponReader.java
@@ -20,6 +20,7 @@ import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.auction.common.constants.BatchConst.CHECK_EXPIRE_COUPON_PAGING_READER;
 import static com.auction.common.constants.BatchConst.SLAVE_DATASOURCE;
 
 @Slf4j
@@ -28,60 +29,6 @@ import static com.auction.common.constants.BatchConst.SLAVE_DATASOURCE;
 public class CheckExpireCouponReader {
     @Value("${spring.batch.job.chunk-size}")
     private int chunkSize;
-
-//    @Bean
-//    @StepScope
-//    public JpaPagingItemReader<CouponUser> getExpireCouponReader(
-//            EntityManagerFactory entityManagerFactory,
-//            @Value("#{jobParameters['expireAt']}") LocalDate expireAt
-//    ) {
-//        HashMap<String, Object> parameters = new HashMap<>();
-//        parameters.put("expireAt", expireAt);
-//
-//        return new JpaPagingItemReaderBuilder<CouponUser>()
-//                .name("couponIdPagingReader")
-//                .pageSize(1000)
-//                .entityManagerFactory(entityManagerFactory)
-//                .queryString("SELECT u FROM Coupon c JOIN CouponUser u ON c.id = u.coupon.id " +
-//                        "WHERE c.expireAt = :expireAt ORDER BY u.id")
-//                .parameterValues(parameters)
-//                .build();
-//    }
-
-//    @Bean
-//    @StepScope
-//    public JdbcPagingItemReader<CouponDto> getExpireCouponReader(
-//            PagingQueryProvider queryProvider,
-//            @Value("#{jobParameters['expireAt']}") LocalDate expireAt
-//    ) {
-//        HashMap<String, Object> parameters = new HashMap<>();
-//        parameters.put("expire_at", expireAt);
-//
-//        return new JdbcPagingItemReaderBuilder<CouponDto>()
-//                .name("couponIdPagingReader")
-//                .pageSize(10000)
-//                .fetchSize(10000)
-//                .dataSource(dataSource)
-//                .queryProvider(queryProvider)
-//                .parameterValues(parameters)
-//                .rowMapper(new BeanPropertyRowMapper<>(CouponDto.class))
-//                .build();
-//    }
-//
-//    @Bean
-//    public PagingQueryProvider queryProvider() throws Exception {
-//        SqlPagingQueryProviderFactoryBean queryProvider = new SqlPagingQueryProviderFactoryBean();
-//        queryProvider.setDataSource(dataSource);
-//        queryProvider.setSelectClause("SELECT u.id");
-//        queryProvider.setFromClause("FROM coupon c JOIN coupon_user u ON c.id = u.coupon_id");
-//        queryProvider.setWhereClause("WHERE c.expire_at = :expire_at AND u.is_available = true");
-//
-//        Map<String, Order> sortKeys = new HashMap<>(1);
-//        sortKeys.put("u.id", Order.ASCENDING);
-//
-//        queryProvider.setSortKeys(sortKeys);
-//        return queryProvider.getObject();
-//    }
 
     @Bean
     @StepScope
@@ -98,7 +45,7 @@ public class CheckExpireCouponReader {
         parameters.put("max_value", maxValue);
 
         return new JdbcPagingItemReaderBuilder<CouponDto>()
-                .name("couponIdPagingReader")
+                .name(CHECK_EXPIRE_COUPON_PAGING_READER)
                 .pageSize(chunkSize)
                 .fetchSize(chunkSize)
                 .dataSource(dataSource)
